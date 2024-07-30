f25c1d6aed0bc3ac69c236396b17f5b60252efc8
Zohre <mohammadizohre1371@gmail.com>
2024-07-30 14:12:43 +0330

    add crud for employee

--- src/main/java/entity/User.java
+++ src/main/java/entity/User.java
@@ -11,15 +11,16 @@ import lombok.Setter;
 @Getter
 @AllArgsConstructor
 @NoArgsConstructor
-@MappedSuperclass
+@MappedSuperclass()
+
 public class User extends BaseEntity{
 
     public static final String FIRST_NAME = "first_Name";
     public static final String LAST_NAME = "last_Name";
     public static final String NATIONAL_CODE = "national_Code";
     public static final String MOBILE_NUMBER = "mobile_Number";
-  //  public static final String USER_TYPE = "user_type";
-
+    public static final String USERNAME="username";
+    public static final String PASSWORD="password";
 
     @Column(name = FIRST_NAME)
     private String firstName;
@@ -33,9 +34,10 @@ public class User extends BaseEntity{
     @Column(name = MOBILE_NUMBER)
     private String mobileNumber;
 
-//    @Enumerated(value = EnumType.STRING)
-//    @Column(name = USER_TYPE)
-//    private UserType userType;
+    @Column(name = USERNAME,nullable = false)
+    private String username;
 
+    @Column(name = PASSWORD,nullable = false)
+    private String password;
 
 }
