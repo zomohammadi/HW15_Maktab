1a5fc4dcfbe8e689d532b5780b81f8db1b717f50
Zohre <mohammadizohre1371@gmail.com>
2024-08-04 03:47:25 +0330

    add persistence.xml

--- src/main/java/Application.java
+++ src/main/java/Application.java
@@ -1,11 +1,10 @@
-import jakarta.persistence.EntityManager;
 import util.ApplicationContext;
 
 public class Application {
 
     public static void main(String[] args) {
         ApplicationContext applicationContext = ApplicationContext.getInstance();
-        EntityManager entityManager = applicationContext.getEntityManager();
+
 
     }
 }
